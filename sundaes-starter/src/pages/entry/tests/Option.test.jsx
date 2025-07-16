a3bd1d9542d3776d7a00cd7e44184c8f79bcfd89
Rafael Moreira Dias <rafael.moreira@afixcode.com.br>
2025-07-16 18:55:14 -0300

    update progress

--- sundaes-starter/src/pages/entry/tests/Option.test.jsx
+++ sundaes-starter/src/pages/entry/tests/Option.test.jsx
@@ -1,16 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 
-import Options from '../Option';
+import Options from "../Option";
 
-test('displays image for each scoop option from server', () => {
-    render(<Options optionType="scoops" />);
+test("displays image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
 
-    //find images
-    const scoopImages = screen.getAllByRole('img' , { name: /scoop$/i });
-    expect(scoopImages).toHaveLength(2);
+  //find images
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
 
-    // confirm alt text of images
-    const altText = scoopImages.map((element) => element.alt);
-    expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
-    
-})
+  // confirm alt text of images
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("displays image for each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  //find images, expect 3 images based on what msw returns
+  const images = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  // confirm alt text of images
+  const imagesTitles = images.map((img) => img.alt);
+  expect(imagesTitles).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot Fudge topping",
+  ]);
+});
--- sundaes-starter/src/setupTests.js
+++ sundaes-starter/src/setupTests.js
@@ -1,8 +1,7 @@
 import "@testing-library/jest-dom";
 
 import { beforeAll, afterEach, afterAll } from 'vitest'
-import { server } from './mocks/server'
-
+import { server } from './mocks/server';
 // Establish API mocking before all tests
 beforeAll(() => server.listen());
 
